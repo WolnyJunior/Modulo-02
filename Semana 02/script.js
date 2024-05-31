1073fd883ea917d965258a17b6e60f89e13bff69
WolnyJunior <soares.junior.dev@gmail.com>
2024-05-31 19:24:47 -0300

    :fly: Semana02 -  Exercicio-03

---
+++ Semana 02/script.js
@@ -0,0 +1,18 @@
+document.getElementById("meu-formulario").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita o recarregamento da página
+
+    // Captura os valores dos campos
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const mensagem = document.getElementById("texto").value;
+
+    // Exibe os valores no console
+    console.log("Nome:", nome);
+    console.log("Email:", email);
+    console.log("Mensagem:", mensagem);
+
+    // Atualiza o conteúdo dos spans com os valores capturados
+    document.getElementById("resultadoNome").innerText = `Nome: ${nome}`;
+    document.getElementById("resultadoEmail").innerText = `Email: ${email}`;
+    document.getElementById("resultadoMsg").innerText = `Mensagem: ${mensagem}`;
+});
