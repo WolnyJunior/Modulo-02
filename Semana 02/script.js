23a9033e3517d954300a03bf10f2cd5a13075d7f
Taline Araújo <taline.araujo@hotmail.com>
2024-06-02 20:38:44 -0300

    Alterações feitas no projeto

--- Semana 02/script.js
+++ Semana 02/script.js
@@ -5,14 +5,17 @@ document.getElementById("meu-formulario").addEventListener("submit", function(ev
     const nome = document.getElementById("nome").value;
     const email = document.getElementById("email").value;
     const mensagem = document.getElementById("texto").value;
+    const endereco = document.getElementById("endereco").value;
 
     // Exibe os valores no console
     console.log("Nome:", nome);
     console.log("Email:", email);
     console.log("Mensagem:", mensagem);
+    console.log("Endereco:", endereco);
 
     // Atualiza o conteúdo dos spans com os valores capturados
     document.getElementById("resultadoNome").innerText = `Nome: ${nome}`;
     document.getElementById("resultadoEmail").innerText = `Email: ${email}`;
     document.getElementById("resultadoMsg").innerText = `Mensagem: ${mensagem}`;
+    document.getElementById("resultadoEndereco").innerText = `Endereco: ${endereco}`;
 });
